2a1af483f17faa1f48c6efd915831792c2cb4586
checky huang <checkyh@live.cn>
2015-11-30 14:30:20 +0800

    Initial Commit

---
+++ Scene_Music/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scene_Music
+//
+//  Created by checkyh on 15/11/30.
+//  Copyright © 2015年 checkyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
